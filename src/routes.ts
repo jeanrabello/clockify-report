380d8aca18853088dfda25151da277c8950ab079
Jean rabello <jean.rabello.prog@gmail.com>
2024-01-09 20:59:08 -0300

    Add postman route

--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,6 @@ import { Router } from 'express';
 import GetClockifyUserController from './controllers/GetClockifyUserController';
 
 const router = Router();
-
 const getClockifyUserController = new GetClockifyUserController();
 
 router.post('/clockify', getClockifyUserController.generateReport);
